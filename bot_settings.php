038660f1d965699a626370f1c3e4152b883d1675
ton308 <ton_308@hotmail.com>
2017-12-30 22:42:44 +0700

    -

---
+++ bot_settings.php
@@ -0,0 +1,6 @@
+<?php
+/// การตั้งค่าเกี่ยวกับ bot ใน LINE Messaging API
+define('LINE_MESSAGE_CHANNEL_ID', '1550546494');
+define('LINE_MESSAGE_CHANNEL_SECRET', 'dba4efb8003d583f9f1b41e57c40dd69');
+define('LINE_MESSAGE_ACCESS_TOKEN', 'SuGGexmrB35P8Ru8maeucpCmVJa0eevHIAEhBlNaeN7FMpF2qEJ+EtYDzuTqHD6OqHFGNddntGYzajnUH6UGqPbJPYX1ZPYgBoT77gyoq9vxfk5mHjZbcTeuAbinImWxnRgmm2i0wxulACBSzlHiQAdB04t89/1O/w1cDnyilFU=');
+?>
